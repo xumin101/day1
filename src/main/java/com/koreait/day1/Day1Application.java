d9ee3c299950d033961d3bd21ca17221bd9d8938
xumin <xumin262@gmail.com>
2020-11-05 00:01:03 +0900

    Day1Application 수정

--- src/main/java/com/koreait/day1/Day1Application.java
+++ src/main/java/com/koreait/day1/Day1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Day1Application {
 
     public static void main(String[] args) {
+        System.out.println("이곳이 시작점");
         SpringApplication.run(Day1Application.class, args);
     }
 
